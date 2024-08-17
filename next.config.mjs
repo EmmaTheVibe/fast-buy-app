2b93efa1e08129c57c3654510e15ca6378f84a10
EmmaTheVibe <peteremmanuel316@gmail.com>
2024-08-17 17:46:45 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'plus.unsplash.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'jpyobjnetgmloycqazlk.supabase.co',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'loremflickr.com',
+        },
+      ],
+    },
+    // typescript: {
+    //   // !! WARN !!
+    //   // Dangerously allow production builds to successfully complete even if
+    //   // your project has type errors.
+    //   // !! WARN !!
+    //   ignoreBuildErrors: true,
+    // },
+  };
+  
+  export default nextConfig;
+  
